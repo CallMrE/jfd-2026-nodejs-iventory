3a8a0445386055d97b04fdb580f6ccbc80c15d76
HanGieTime <elgykputra155@gmail.com>
2026-02-27 22:17:20 +0700

    list view & list produk

---
+++ model/m_produk.js
@@ -0,0 +1,26 @@
+const mysql = require('mysql2')
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'jfd_jan2026'
+})
+db.connect()
+
+module.exports =
+{
+  get_semua_produk: function (form_username) {
+       let sql = mysql.format(
+                'SELECT * FROM master_produk',[]
+            )
+            return new Promise( function(resolve,reject) {
+                db.query(sql, function(errorSql, hasil) {
+                    if (errorSql) {
+                        reject(errorSql)
+                    } else {
+                        resolve(hasil)
+                    }
+                })
+            })
+  }
+}
